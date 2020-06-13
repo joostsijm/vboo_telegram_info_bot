83419211affb336be01774233b952a3749e3fc4c
JoostSijm <joostsijm@gmail.com>
2020-06-14 00:56:21 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# VBOO Info Bot
+This Telegram bot is designed to convenient get data from Rival Regions.
+More information will follow.
---
+++ setup.py
@@ -0,0 +1,29 @@
+"""Setup file"""
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+setuptools.setup(
+    name="vboo_info_bot",
+    version="0.0.1",
+    author="Joost Sijm",
+    author_email="joostsijm@gmail.com",
+    description="VBOO Telegram info bot for Rival Regions",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="gogs@git.craftbroec.nl:joostsijm/vboo_info_bot.git",
+    packages=setuptools.find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        "python-telegram-bot",
+        "python-dotenv",
+        "hyperlink",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+)
